8f97104fca609bcb3dd998e46bdb9f1059974645
JuhiNagpure <juhinagpure143@gmail.com>
2022-06-19 19:21:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,189 @@
+// 1. ways to print in JavaScript
+// console.log("hello world")
+//alert("me");
+// document.write("this is document write")
+
+// 2. JavaScript console API
+// console.log("hello world")
+// alert("me");
+// document.write("this is document write")
+// console.warning("this is warning")
+// console.error("this is an error")
+
+// 3.JavaScript variables
+// what are variables? - containers to store data values.
+
+// var number = 34;
+// var number2 = 56;
+// console.log(number+number2);
+
+// // 4. Data types in JavaScript
+// // Numbers
+// var num1 = 455;
+// var num2 = 56.76;
+
+// // String
+// var str1 = "this is a string";
+// var str2 = "this is also a string";
+
+// // objects
+// var marks = {
+//     Atharv:99.97,
+//     Juhi:34,
+//     Anushka:78.
+// }
+// console.log(marks);
+
+// // Booleans
+// var a = true;
+// var b = false;;
+// console.log(a,b);
+
+// var und = undefined;
+// var und
+// console.log(und);
+
+// var n = null;
+// console.log(n);
+
+// /*
+// At a very high level , there are two types of data types in JavaScript.
+// 1. Primitive data types:undefined,null,number,string,boolean,symbol.
+// 2. Reference data types:Arrays and objects.
+// */
+
+// // Arrays
+// var arr = [1,2,"juhi",4,5]
+// console.log(arr);
+
+// // operators in JavaScript
+// // Arithmetic operators
+// var a = 34;
+// var b = 56;
+// console.log("The value of a + b is",a+b);
+// console.log("The value of a - b is",a-b);
+// console.log("The value of a * b is",a*b);
+// console.log("The value of a / b is",a/b);
+
+// // Asignment Operators
+// var c = b;
+// // c += 2;
+// // c -= 2;  //c=c-2;
+// // c *= 2;
+// // c /= 2;
+// // console.log(c);
+
+// // comparison operators
+// var x = 34;
+// var y = 56;
+// // console.log(x == y);
+// // console.log(x >= y);
+// // console.log(x <= y);
+// // console.log(x > y);
+// // console.log(x < y);
+
+// // logical operators
+// // logical and
+// //  console.log(true && true)
+// //  console.log(true && false)
+// //  console.log(false && true)
+// //  console.log(false && false)
+
+// // logical or
+// //  console.log(true || true)
+// //  console.log(true  || false)
+// //  console.log(false  || true)
+// //  console.log(false || false)
+
+// // logical not
+// // console.log(!false);
+// // console.log(!true);
+// function avg(a, b){
+//     c = (a + b)/2;
+//     return c;
+// }
+
+// c1 = avg(4, 6);
+// c2 = avg(14,16);
+// console.log(c1,c2);
+
+// conditionals in JavaScript
+
+// var age = 25;
+// // single if statement
+//  if(age > 8){
+//      console.log("you are not a kid"); }
+
+// if - else statement
+// if(age > 8){
+//     console.log("you are not a kid");
+// }
+// else{
+//     console.log("you are a kid")
+// }
+
+// if - else ladder
+// if(age > 32){
+//     console.log("you are not a kid")
+// }
+// else if(age > 26){
+//     console.log("bacche nhi rahe");
+// }
+// else if(age > 22){
+//     console.log(" yes bacche nhi rahe");
+// }
+// else if(age > 18){
+//     console.log("18 bacche nhi rahe");
+// }
+// else{
+//     console.log("bacche rahe");
+// }
+// console.log("End of ladder");
+
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// // console.log(arr);
+// for(var i = 0; i<arr.length;i++){
+// if(i==2){
+//     // break
+//     continue;
+// }
+
+
+//     console.log(arr[i])
+// }
+
+// arr.forEach(function(element){
+//     console.log(element);
+// })
+
+// const ac = 0;
+// ac++;
+// ac = ac + 1;
+// let j = 0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+
+// do{
+//     console.log(arr[j]);
+//     j++;
+//   }  while(j < arr.length);
+
+
+// let myArr = ["fan","camera",34,null,true];
+// // Array Methods
+// console.log(myArr.length);
+// // myArr.pop();
+// // myArr.push("harry")
+// // myArr.shift()
+// myArr.unshift("harry")
+// const newlen = myArr.unshift("harry")
+// console.log(newlen)
+// console.log(myArr);
+
+// string methods in JavaScript 
+let mylovelystring = "Harry is a good boy";
+console.log(mylovelystring.length)
+console.log(mylovelystring.indexOf(""))
+
